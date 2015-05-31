7e82b75bc5b8bd51e9211af5faf77b3c0f072336
Talha Sayed <talha_sayed@hotmail.com>
2015-05-31 22:30:18 +0300

    Change .gitignore to include the makefile and .mk files

---
+++ Debug/src/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/game.cpp \
+../src/main.cpp 
+
+OBJS += \
+./src/game.o \
+./src/main.o 
+
+CPP_DEPS += \
+./src/game.d \
+./src/main.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -I"E:\workspace\MiniPhysx\include\mingw_devlib\include\SDL2" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
